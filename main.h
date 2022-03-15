325466c084c7df18eb122c82b88a3abef867ceff
faroukbmiled <fcfaroukfc@gmail.com>
2022-03-15 14:56:33 -0700

    got gix

--- main.h
+++ main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * struct typeprint - create libraries
  * @type: char
